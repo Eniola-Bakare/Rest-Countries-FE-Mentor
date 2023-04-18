016c697a23ea88e83e6be19ea15f57dc041c3c1a
Eniola Bakare <eebakare@gmail.com>
2023-04-18 23:40:30 +0100

    working on the back btn

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const themeIcon = document.querySelector('.theme-icon')
 const body = document.querySelector('body');
 const allCountryWrapper = document.querySelector('.all-countries-wrapper')
 const searchBarInput = document.querySelector('.search-input')
+const searchContainer = document.querySelector('.search-container')
 const selectedFilterVal = document.querySelector('#region-filter')
 
 
@@ -16,102 +17,109 @@ let region = ''
 let regionArr = []
 let regionArrfilter = []
 
-const updateUI = function(data){
-  allCountryWrapper.innerHTML = ''
-  data.map(eachCountry => {
-    return allCountryWrapper.insertAdjacentHTML('beforeend', `
-      <section class="country-card">
-        
+// detailsFunc 
+const detailsFunc = function(eachCountryCard){
 
-        <div class="country-info">
-          <h4 class="country-name">${eachCountry.name}</h4>
-          <p class="country-population"><span class="info-item">Population: </span>${eachCountry.population}</p>
-          <p class="country-region"><span class="info-item">Region: </span>${eachCountry.region}</p>
-          <p class="country-capital"><span class="info-item">Capital: </span>${eachCountry.subregion}</p>
+  return eachCountryCard.addEventListener('click', function(){
+    const countryName = eachCountryCard.childNodes[1].textContent
+    const matchingCountry = dataJSON.filter(eachCountry => {
+      console.log(eachCountry.name.toLowerCase() === countryName.toLowerCase())
+      return eachCountry.name.toLowerCase() === countryName.toLowerCase()
+    })
+    for (const country of matchingCountry) {
+      body.innerHTML = '';
+   
+      body.insertAdjacentHTML('beforeend', `
+      <header class="header">
+        <h1 class="header-title">Where in the world?</h1>
+        <p class="theme-icon"><i class="fa-sharp fa-solid fa-moon"></i> Dark Mode</p>
+      </header>
+      <section class="back-btn">
+        <button>
+          <i class="fa-sharp fa-solid fa-arrow-left"></i> Back
+        </button>
+      </section> 
+
+      <section class="country-details">
+        <!--<img src=${country.flags.png} alt= ${country.name +'s' + ' flag'} >-->
+
+        <div class="details">
+          <h2 class="details-country-name">${country.name}</h2>
+          <div class="extra-details">
+            <div class="detail-left">
+              <p class="detail-item">
+                <span class="detail-type">Native Name: </span> ${country.nativeName}
+              </p>
+              <p class="detail-item">
+                <span class="detail-type">Population: </span> ${country.population}
+              </p>
+              <p class="detail-item">
+                <span class="detail-type">Region: </span> ${country.region}
+              </p>
+              <p class="detail-item">
+                <span class="detail-type">Sub Region: </span> ${country.subregion}
+              </p>
+              <p class="detail-item">
+                <span class="detail-type">Capital: </span> ${country.capital}
+              </p>
+            </div>
+
+            <div class="detail-right">
+              <p class="detail-item">
+                <span class="detail-type">Top Level Domain: </span> ${country.topLevelDomain}
+              </p>
+              <p class="detail-item">
+                <span class="detail-type">Currencies: </span> ${country.currencies[0].code}
+              </p>
+              <p class="detail-item">
+                <span class="detail-type">Languages: </span> ${country.languages.map(lang => lang.name).join(', ')}
+              </p>                    
+            </div>
+          </div>
+
+          <div class="border-countries">
+            <p class="border-countries-para">Border Countries:</p>
+            ${//console.log(country?.borders)
+              country.borders?.map(eachBorder => {
+                return `<button class="border-countries-btn">
+                  ${eachBorder}
+                </button>`
+              }).join('')
+            }
+
         </div>
       </section>
+      `)
+      // backbtn
+
+      document.querySelector('.back-btn').addEventListener('click', console.log('here'))
+    }
+  })
+}
+
+const updateUI = function(data){
+  allCountryWrapper.innerHTML =''
+  data.map(eachCountry => {
+    return allCountryWrapper.insertAdjacentHTML('beforeend', `
+    <section class="country-card">
+      
+
+      <div class="country-info">
+        <h4 class="country-name">${eachCountry.name}</h4>
+        <p class="country-population"><span class="info-item">Population: </span>${eachCountry.population}</p>
+        <p class="country-region"><span class="info-item">Region: </span>${eachCountry.region}</p>
+        <p class="country-capital"><span class="info-item">Capital: </span>${eachCountry.subregion}</p>
+      </div>
+    </section>
     `)
   })
 
-  const countryCardAll = document.querySelectorAll('.country-card')
-    countryCardAll.forEach(eachCountryCard => {
-      return eachCountryCard.addEventListener('click', function(){
-        console.log(eachCountryCard.children.childNodes)
-        // dataJSON.filter(getCountry => {
-        //   eachCountryCard.children
-        // })
-        //-----
-        // body.innerHTML = '';
-        // body.insertAdjacentHTML('beforeend', `
-        // <header class="header">
-        //   <h1 class="header-title">Where in the world?</h1>
-        //   <p class="theme-icon"><i class="fa-sharp fa-solid fa-moon"></i> Dark Mode</p>
-        // </header>
-        // <section class="back-btn">
-        //   <button>
-        //     <i class="fa-sharp fa-solid fa-arrow-left"></i> Back
-        //   </button>
-        // </section>
-
-        // <section class="country-details">
-        //   <img src="./germ-img.png" alt="">
-
-        //   <div class="details">
-        //     <h2 class="details-country-name">Belgium</h2>
-        //     <div class="extra-details">
-        //       <div class="detail-left">
-        //         <p class="detail-item">
-        //           <span class="detail-type">Native Name: </span> Belge
-        //         </p>
-        //         <p class="detail-item">
-        //           <span class="detail-type">Population: </span> 11, 319 511
-        //         </p>
-        //         <p class="detail-item">
-        //           <span class="detail-type">Region: </span> Europe
-        //         </p>
-        //         <p class="detail-item">
-        //           <span class="detail-type">Sub Region: </span> Western Europe
-        //         </p>
-        //         <p class="detail-item">
-        //           <span class="detail-type">Capital: </span> Brussels
-        //         </p>
-        //       </div>
-
-        //       <div class="detail-right">
-        //         <p class="detail-item">
-        //           <span class="detail-type">Top Level Domain: </span> be
-        //         </p>
-        //         <p class="detail-item">
-        //           <span class="detail-type">Currencies: </span> Euro
-        //         </p>
-        //         <p class="detail-item">
-        //           <span class="detail-type">Languages: </span> Dutch, French, German
-        //         </p>
-        //       </div>
-        //     </div>
-
-        //     <div class="border-countries">
-        //       <p class="border-countries-para">Border Countries:</p>
-        //       <button class="border-countries-btn">
-        //         Netherlands
-        //       </button>
-        //       <button class="border-countries-btn">
-        //         France
-        //       </button>
-        //       <button class="border-countries-btn">
-        //         Germany
-        //       </button>
-        //     </div>
-        //   </div>
-        // </section>
-        // `)
-
-      })
-    })
-  // const countryDetailsWrapper = document.querySelector('.country-details')
-}
-
-updateUI(dataJSON)
+    document.querySelectorAll('.country-card > *').forEach((eachCountryCard ) => detailsFunc(eachCountryCard))
+  }
+  
+  updateUI(dataJSON)
+  // const backBtn = document.querySelector('.back-btn')
+  // backBtn.addEventListener('click', updateUI(dataJSON))
 
 // filter by country name
 const forCountry = function(dataArr){
@@ -123,7 +131,7 @@ const forCountry = function(dataArr){
     eachCountry.name.toLowerCase().includes(countryTextInput.toLowerCase())
 })
 }
-
+const goBackFunc = function(){}
 
 //
 const filterByCountryNameFunc = function(){
@@ -169,4 +177,10 @@ selectedFilterVal.addEventListener('change', function(){
 
   // allCountryWrapper.innerHTML = ''
   return updateUI(regionArr)
+})
+
+
+
+themeIcon.addEventListener('click', function(){
+  console.log(body.childNodes)
 })
