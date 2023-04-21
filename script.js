f3e8d5a9cc99c39bb7d6bd52019cc36079651aea
Eniola Bakare <eebakare@gmail.com>
2023-04-21 15:19:49 +0100

    implemented border botton click through

--- script.js
+++ script.js
@@ -18,6 +18,98 @@ let regionArr = []
 let regionArrfilter = []
 let themeState = false;
 
+const detailsFunc = function(country){
+  allCountryWrapper.innerHTML =''
+  if (document.querySelector('.country-details')) {
+    document.querySelector('.country-details').remove()
+  }
+   body.insertAdjacentHTML('beforeend', `
+  
+          <section class="country-details">
+            <img src=${country.flags.png} alt= ${country.name +'s' + ' flag'} >
+  
+            <div class="details">
+              <h2 class="details-country-name">${country.name}</h2>
+              <div class="extra-details">
+                <div class="detail-left">
+                  <p class="detail-item">
+                    <span class="detail-type">Native Name: </span> ${country.nativeName}
+                  </p>
+                  <p class="detail-item">
+                    <span class="detail-type">Population: </span> ${country.population}
+                  </p>
+                  <p class="detail-item">
+                    <span class="detail-type">Region: </span> ${country.region}
+                  </p>
+                  <p class="detail-item">
+                    <span class="detail-type">Sub Region: </span> ${country.subregion}
+                  </p>
+                  <p class="detail-item">
+                    <span class="detail-type">Capital: </span> ${country.capital}
+                  </p>
+                </div>
+  
+                <div class="detail-right">
+                  <p class="detail-item">
+                    <span class="detail-type">Top Level Domain: </span> ${country.topLevelDomain}
+                  </p>
+                  <p class="detail-item">
+                    <span class="detail-type">Currencies: </span> ${country.currencies?.map(each => {each.code})}
+                  </p>
+                  <p class="detail-item">
+                    <span class="detail-type">Languages: </span> ${country.languages.map(lang => lang.name).join(', ')}
+                  </p>                    
+                </div>
+              </div>
+  
+              <div class="border-countries">
+                <p class="border-countries-para">Border Countries:</p>
+                ${//console.log(country?.borders)
+                  country.borders?.map(eachBorder => {
+                    return `<button class="border-countries-btn">
+                      ${eachBorder}
+                    </button>`
+                  }).join('')
+                }
+
+            </div>
+          </section>
+          `)
+
+          // for borders
+        document.querySelectorAll('.border-countries-btn').forEach(border => {
+          border.addEventListener('click', function(){
+            let borderMatch = border.textContent.toLowerCase().trim()
+            const borerMatched = dataJSON.filter(country => {
+              return country.alpha3Code.toLowerCase() === borderMatch
+            })
+            for (const country of borerMatched) {
+              detailsFunc(country)
+            }
+          })
+        })
+         // for borders theme
+         if(!themeState){
+          document.querySelectorAll('.border-countries-btn').forEach(each => {
+            each.style.color = 'var(--white-Dark-Mode-Text--Light-Mode-Elements)';
+            each.style.backgroundColor =  'var(--very-dark-Blue-Light-Mode-Text)';
+           
+          })
+          document.querySelectorAll('.detail-type').forEach(each => {
+            each.style.color = 'var(--white-Dark-Mode-Text--Light-Mode-Elements)';
+          })
+        }else if(themeState){
+          document.querySelectorAll('.border-countries-btn').forEach(each => {
+            each.style.color = 'var(--very-dark-Blue-Light-Mode-Text)';
+            each.style.backgroundColor =  'var(--white-Dark-Mode-Text--Light-Mode-Elements)';
+          })
+          document.querySelectorAll('.detail-type').forEach(each => {
+            each.style.color = 'var(--very-dark-Blue-Light-Mode-Text)';
+          })
+        }
+
+}
+
 const themeFuncElem = function(colorElems, forBody, colorText){
   const header = document.querySelector('.header');
   const searchIcon = document.querySelector('.fa-solid')
@@ -69,131 +161,56 @@ const styleFuncShow = function(elem){
 const updateUI = function(data){
   styleFuncHide(backBTN)
 
-  styleFuncShow(allCountryWrapper)
+    styleFuncShow(allCountryWrapper)
 
-  styleFuncShow(searchContainer)
+    styleFuncShow(searchContainer)
 
-  if (document.querySelector('.country-details')) {
-    console.log('country details here____')
-    document.querySelector('.country-details').remove()
-  }
+    if (document.querySelector('.country-details')) {
+      console.log('country details here____')
+      document.querySelector('.country-details').remove()
+    }
 
-  allCountryWrapper.innerHTML =''
-  data.map(eachCountry => {
-    return allCountryWrapper.insertAdjacentHTML('beforeend', `
-    <section class="country-card">
-      
-    <img loading="lazy" src= ${eachCountry.flags.png} alt="${eachCountry.name} flag image">
-      <div class="country-info">
-        <h4 class="country-name">${eachCountry.name}</h4>
-        <p class="country-population"><span class="info-item">Population: </span>${eachCountry.population}</p>
-        <p class="country-region"><span class="info-item">Region: </span>${eachCountry.region}</p>
-        <p class="country-capital"><span class="info-item">Capital: </span>${eachCountry.subregion}</p>
-      </div>
-    </section>
-    `)
+    allCountryWrapper.innerHTML =''
+    data.map(eachCountry => {
+      return allCountryWrapper.insertAdjacentHTML('beforeend', `
+      <section class="country-card">
+        
+      <img loading="lazy" src= ${eachCountry.flags.png} alt="${eachCountry.name} flag image">
+        <div class="country-info">
+          <h4 class="country-name">${eachCountry.name}</h4>
+          <p class="country-population"><span class="info-item">Population: </span>${eachCountry.population}</p>
+          <p class="country-region"><span class="info-item">Region: </span>${eachCountry.region}</p>
+          <p class="country-capital"><span class="info-item">Capital: </span>${eachCountry.subregion}</p>
+        </div>
+      </section>
+      `)
   })
+
+
  
-    document.querySelectorAll('.country-card > *').forEach(eachCountryCard => {
-    return eachCountryCard.addEventListener('click', function(){
+    document.querySelectorAll('.country-card ').forEach(eachCountryCard => {
+    return eachCountryCard.addEventListener('click', function(e){
+      let test = eachCountryCard.childNodes[3]
+      
       styleFuncShow(backBTN)
 
      styleFuncHide(allCountryWrapper)
 
       styleFuncHide(searchContainer)
 
-        const countryName = eachCountryCard.childNodes[1].textContent
+        // const countryName = eachCountryCard.childNodes[2].textContent
+        const countryName = test.childNodes[1].textContent.toLowerCase();
         const matchingCountry = dataJSON.filter(eachCountry => {
-          // console.log(eachCountry.name.toLowerCase() === countryName.toLowerCase())
           return eachCountry.name.toLowerCase() === countryName.toLowerCase()
         })
         for (const country of matchingCountry) {
-          allCountryWrapper.innerHTML =''
-          body.insertAdjacentHTML('beforeend', `
-  
-          <section class="country-details">
-            <!--<img src=${country.flags.png} alt= ${country.name +'s' + ' flag'} >-->
-  
-            <div class="details">
-              <h2 class="details-country-name">${country.name}</h2>
-              <div class="extra-details">
-                <div class="detail-left">
-                  <p class="detail-item">
-                    <span class="detail-type">Native Name: </span> ${country.nativeName}
-                  </p>
-                  <p class="detail-item">
-                    <span class="detail-type">Population: </span> ${country.population}
-                  </p>
-                  <p class="detail-item">
-                    <span class="detail-type">Region: </span> ${country.region}
-                  </p>
-                  <p class="detail-item">
-                    <span class="detail-type">Sub Region: </span> ${country.subregion}
-                  </p>
-                  <p class="detail-item">
-                    <span class="detail-type">Capital: </span> ${country.capital}
-                  </p>
-                </div>
-  
-                <div class="detail-right">
-                  <p class="detail-item">
-                    <span class="detail-type">Top Level Domain: </span> ${country.topLevelDomain}
-                  </p>
-                  <p class="detail-item">
-                    <span class="detail-type">Currencies: </span> ${country.currencies?.map(each => {each.code})}
-                  </p>
-                  <p class="detail-item">
-                    <span class="detail-type">Languages: </span> ${country.languages.map(lang => lang.name).join(', ')}
-                  </p>                    
-                </div>
-              </div>
-  
-              <div class="border-countries">
-                <p class="border-countries-para">Border Countries:</p>
-                ${//console.log(country?.borders)
-                  country.borders?.map(eachBorder => {
-                    return `<button class="border-countries-btn">
-                      ${eachBorder}
-                    </button>`
-                  }).join('')
-                }
-
-            </div>
-          </section>
-          `)
-          // backbtn
-        }
-        if(!themeState){
-          document.querySelectorAll('.border-countries-btn').forEach(each => {
-            each.style.color = 'var(--white-Dark-Mode-Text--Light-Mode-Elements)';
-            each.style.backgroundColor =  'var(--very-dark-Blue-Light-Mode-Text)';
-           
-          })
-          document.querySelectorAll('.detail-type').forEach(each => {
-            each.style.color = 'var(--white-Dark-Mode-Text--Light-Mode-Elements)';
-          })
-        }else if(themeState){
-          document.querySelectorAll('.border-countries-btn').forEach(each => {
-            each.style.color = 'var(--very-dark-Blue-Light-Mode-Text)';
-            each.style.backgroundColor =  'var(--white-Dark-Mode-Text--Light-Mode-Elements)';
-          })
-          document.querySelectorAll('.detail-type').forEach(each => {
-            each.style.color = 'var(--very-dark-Blue-Light-Mode-Text)';
-          })
+          detailsFunc(country)
         }
 
         backBTN.addEventListener('click', function(){updateUI(dataJSON)})
     })
   })
    if(!themeState){
-    const header = document.querySelector('.header');
-    const searchIcon = document.querySelector('.fa-solid')
-    const searchInputPlaceHolder = document.querySelector('.search-input')
-    const infoType = document.querySelectorAll('.info-item')
-    const borderCountries = document.querySelectorAll('.broder-countries-btn')
-    searchBar;
-    selectedFilterVal;
-    body;
     themeFuncElem('var(--dark-blue-Dark-Mode-Elements)', 'var(--very-dark-blue-Dark-Mode-Background)', 'var(--white-Dark-Mode-Text--Light-Mode-Elements)') 
   }else if(themeState){
     themeFuncElem('var(--white-Dark-Mode-Text--Light-Mode-Elements)', 'var(--very-light-gray-Light-Mode-Background', 'var(--very-dark-Blue-Light-Mode-Text)')
@@ -262,23 +279,12 @@ themeIcon.addEventListener('click', function(){
     console.log(themeState)
     // for elems
     searchBar.classList.add('active')
-    // document.querySelectorAll('.border-countries-btn').forEach(each => {
-    //   console.log('not here')
-    //   each.style.color = 'var(--dark-blue-Dark-Mode-Elements)';
-    //   each.style.backgroundColor =  'var(--white-Dark-Mode-Text--Light-Mode-Elements)';
-    // })
     themeFuncElem('var(--white-Dark-Mode-Text--Light-Mode-Elements)', 'var(--very-light-gray-Light-Mode-Background', 'var(--very-dark-Blue-Light-Mode-Text)')
      themeState = !themeState
 
     }else if (themeState) {
       console.log(themeState)
-      // for elems
         searchBar.classList.remove('active')
-        // document.querySelectorAll('.border-countries-btn').forEach(each => {
-        //   console.log('here')
-        //   each.style.color = 'var(--white-Dark-Mode-Text--Light-Mode-Elements)';
-        //   each.style.backgroundColor =  'var(--very-dark-Blue-Light-Mode-Text)';
-        // })
         themeFuncElem('var(--dark-blue-Dark-Mode-Elements)', 'var(--very-dark-blue-Dark-Mode-Background)', 'var(--white-Dark-Mode-Text--Light-Mode-Elements)') 
         themeState = !themeState
     }
